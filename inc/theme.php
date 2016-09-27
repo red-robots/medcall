f0d5d3642e4022bb3a94a99121b73654890ceb6f
Austin Crane <austincrane@gmail.com>
2016-09-27 11:06:09 -0400

    remove posts

--- inc/theme.php
+++ inc/theme.php
@@ -130,33 +130,10 @@ add_action( 'init', 'my_theme_add_editor_styles' );
 /*-------------------------------------
   Change Admin Labels
 ---------------------------------------*/
-function change_post_menu_label() {
-    global $menu;
-    global $submenu;
-    $menu[5][0] = 'News';
-    $submenu['edit.php'][5][0] = 'News';
-    $submenu['edit.php'][10][0] = 'Add News Item';
-    //$submenu['edit.php'][15][0] = 'Status'; // Change name for categories
-    //$submenu['edit.php'][16][0] = 'Labels'; // Change name for tags
-    echo '';
+function custom_menu_page_removing() {
+    remove_menu_page( 'edit.php' );
 }
-
-function change_post_object_label() {
-        global $wp_post_types;
-        $labels = &$wp_post_types['post']->labels;
-        $labels->name = 'News';
-        $labels->singular_name = 'News Item';
-        $labels->add_new = 'Add News Item';
-        $labels->add_new_item = 'Add News Item';
-        $labels->edit_item = 'Edit News Item';
-        $labels->new_item = 'News Item';
-        $labels->view_item = 'View News Item';
-        $labels->search_items = 'Search News';
-        $labels->not_found = 'No News found';
-        $labels->not_found_in_trash = 'No News found in Trash';
-    }
-add_action( 'init', 'change_post_object_label' );
-add_action( 'admin_menu', 'change_post_menu_label' );
+add_action( 'admin_menu', 'custom_menu_page_removing' );
 
 /*-------------------------------------
   Add a last and first menu class option
