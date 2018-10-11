cb06d6e66802dabeaad60d528c330e9dfab8ca53
Austin Crane <austincrane@gmail.com>
2018-10-11 14:45:56 -0400

    align videos

--- page-what-we-do.php
+++ page-what-we-do.php
@@ -17,78 +17,65 @@ if ( has_post_thumbnail() ) {
 ?>
 <div id="page-contents">
 
-<div class="page-title">
-	<div class="wrapper">
-		<header class="entry-header">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		</header><!-- .entry-header -->
-	</div>
-</div>
-
-<div class="wrapper">
-
-
-
-	<div id="primary" class="right-page">
-		<main id="main" class="site-main" role="main">
-
-			<?php
-			while ( have_posts() ) : the_post(); 
-
-				$video = get_field('side_video');
-
-			?>
-
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<div class="entry-content">
-						<?php the_content(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-## -->
-
-				<?php if( is_page('sitemap') ) {
-					wp_nav_menu( array( 'theme_location' => 'sitemap' ) );
-				}
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	<div class="left-page">
-	<?php
-
-	if ( has_post_thumbnail() ) { ?>
-		
-			<div class="side-image">
-				<?php the_post_thumbnail('full') ?>
-			</div>
-			<?php 
-
-			$images = get_field('extra_images');
-
-			if( $images ): ?>
-			    
-			        <?php foreach( $images as $image ): ?>
-			       		<div class="side-image">
-			                <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
-			            </div>
-			          
-			        <?php endforeach; ?>
-			   
-			<?php endif; ?>
-		
-
-	<?php } 
-
-		if( $video ) { ?>
-		<div class="side-video">
-			<?php the_field('side_video'); ?>
+	<div class="page-title">
+		<div class="wrapper">
+			<header class="entry-header">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</header><!-- .entry-header -->
 		</div>
-		<?php }
-
-	?>
 	</div>
-</div>
+
+	<div class="wrapper">
+		<div id="primary" class="">
+			<main id="main" class="site-main" role="main">
+
+				<?php
+				while ( have_posts() ) : the_post(); 
+
+					$video = get_field('side_video');
+					$rightvideo = get_field('side_video_right'); ?>
+
+					<div class="left-page js-blocks">
+
+						<?php if ( has_post_thumbnail() ) { ?>
+							
+								<div class="side-image">
+									<?php the_post_thumbnail('full') ?>
+								</div>
+								
+						<?php } ?>
+					</div>
+					<div class="right-page js-blocks">
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="entry-content">
+								<?php the_content(); ?>
+							</div><!-- .entry-content -->
+						</article><!-- #post-## -->
+					</div>
+
+					<div class="left-page  js-blocks">
+						<?php if( $video ) { ?>
+							<div class="side-video">
+								<?php the_field('side_video'); ?>
+							</div>
+							<?php } ?>
+					</div>	
+
+					<div class="right-page js-blocks">
+						<?php if( $rightvideo ) { ?>
+							<div class="side-video">
+								<?php the_field('side_video_right'); ?>
+							</div>
+							<?php } ?>
+					</div>		
+
+					
+
+					<?php  endwhile; // End of the loop. ?>
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div>
 </div><!-- page -->
 
 <?php 
